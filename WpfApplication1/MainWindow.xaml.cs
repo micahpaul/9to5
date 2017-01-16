4bc4f2cf70dd2c7dc47cedd287a980955a8d378d
micahpaul <mhawkinson@kalos-inc.com>
2017-01-15 20:12:55 -0600

    Add files via upload

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Data.SQLite;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public class DBConnection
+    {
+        private const String DbFileName = "9To5.sqlite";
+        private SQLiteConnection Connection;
+
+        public DBConnection()
+        {
+            if (!File.Exists(DbFileName))
+            {
+                SQLiteConnection.CreateFile(DbFileName);
+            }
+
+            Connection = new SQLiteConnection("Data Source=" + DbFileName + ";Version=3;");
+        }
+
+        public void InitializeTables(List<FieldListItem> _List)
+        {
+            try
+            {
+                Connection.Open();
+                String nl = "\r\n";
+                String Sql  = "CREATE TABLE IF NOT EXISTS TXN (" + nl;
+                Sql += "ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE," + nl;
+                Sql += "ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE," + nl;
+            }
+            catch( Exception e)
+            {
+                Console.WriteLine("InitializeTables() Error: {0}", e.Message);
+            }
+            finally
+            {
+                if ( Connection != null && Connection.State != System.Data.ConnectionState.Closed )
+                {
+                    Connection.Close();
+                }
+            }
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        DBConnection DBConnection = new DBConnection();
+        private List<FieldListItem> FieldList = new List<FieldListItem>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            BuildFieldList();
+            //InitializeDB();
+        }
+
+        private void BuildFieldList()
+        {
+            FieldList.Add(new FieldListItem("First Name", "CST_FNAME", FieldType.ftString));
+            FieldList.Add(new FieldListItem("Last Name", "CST_LNAME", FieldType.ftString));
+            FieldList.Add(new FieldListItem("Address 1", "CST_ADDRESS_1", FieldType.ftString));
+            FieldList.Add(new FieldListItem("Address 2", "CST_ADDRESS_2", FieldType.ftString));
+            FieldList.Add(new FieldListItem("City", "CST_CITY", FieldType.ftString));
+            FieldList.Add(new FieldListItem("State", "CST_STATE", FieldType.ftString));
+            FieldList.Add(new FieldListItem("Zip", "CST_ZIP", FieldType.ftString));
+            FieldList.Add(new FieldListItem("Phone Number", "CST_PHONE", FieldType.ftString));
+            FieldList.Add(new FieldListItem("Warranty Coverage", "WAR_COVERAGE", FieldType.ftReal));
+            FieldList.Add(new FieldListItem("Warranty Period", "WAR_PERIOD", FieldType.ftString));
+            FieldList.Add(new FieldListItem("Warranty Miles", "WAR_MILES", FieldType.ftInt));
+            FieldList.Add(new FieldListItem("Warranty Deductible", "WAR_DEDUCTIBLE", FieldType.ftReal));
+            FieldList.Add(new FieldListItem("VIN", "VEH_VIN", FieldType.ftString));
+            FieldList.Add(new FieldListItem("Year", "VEH_YEAR", FieldType.ftInt));
+            FieldList.Add(new FieldListItem("Make", "VEH_MAKE", FieldType.ftString));
+            FieldList.Add(new FieldListItem("Model", "VEH_MODEL", FieldType.ftString));
+            FieldList.Add(new FieldListItem("Mileage", "VEH_MILEAGE", FieldType.ftReal));
+            FieldList.Add(new FieldListItem("Trim", "VEH_TRIM", FieldType.ftString));
+            FieldList.Add(new FieldListItem("Date of Sale", "SAL_DATE", FieldType.ftDate));
+            FieldList.Add(new FieldListItem("Sale Amount", "SAL_AMOUNT", FieldType.ftReal));
+            FieldList.Add(new FieldListItem("Trade-In Amount", "SAL_TRADE_AMT", FieldType.ftReal));
+            ComboBox box = new ComboBox();
+            SalesTypeBinder Binder = new SalesTypeBinder(box);
+            FieldList.Add(new FieldListItem("Type of Sale", "SAL_TYPE", FieldType.ftChoice, box));
+        }
+
+        private void SaleButton_Click(object sender, RoutedEventArgs e)
+        {
+            SalesWindow salesWindow = new SalesWindow(FieldList);
+            salesWindow.Owner = this;
+            salesWindow.ShowDialog();
+        }
+    }
+}
---
+++ WpfApplication1/SalesWindow.xaml.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+
+    // Visual auxilliary
+    public class MarginSetter
+    {
+        public static Thickness GetMargin(DependencyObject obj)
+        {
+            return (Thickness)obj.GetValue(MarginProperty);
+        }
+
+        public static void SetMargin(DependencyObject obj, Thickness value)
+        {
+            obj.SetValue(MarginProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Margin.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MarginProperty =
+            DependencyProperty.RegisterAttached("Margin", typeof(Thickness), typeof(MarginSetter), new UIPropertyMetadata(new Thickness(), MarginChangedCallback));
+
+        public static void MarginChangedCallback(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            // Make sure this is put on a panel
+            var panel = sender as Panel;
+
+            if (panel == null) return;
+
+
+            panel.Loaded += new RoutedEventHandler(panel_Loaded);
+
+        }
+
+        static void panel_Loaded(object sender, RoutedEventArgs e)
+        {
+            var panel = sender as Panel;
+
+            // Go over the children and set margin for them:
+            foreach (var child in panel.Children)
+            {
+                var fe = child as FrameworkElement;
+
+                if (fe == null) continue;
+
+                fe.Margin = MarginSetter.GetMargin(panel);
+            }
+        }
+    }
+
+    // Non-Visual
+    public enum FieldType { ftUndefined, ftString, ftInt, ftReal, ftDate, ftChoice };
+    
+    // Non-Visual
+    public enum SaleType { Retail, Wholesale, RetailOutOfState };
+
+    public struct SaleTypeInfo
+    {
+        public SaleType Type;
+        public char DBCode;
+        public String FriendlyName;
+
+        public SaleTypeInfo(SaleType _Type)
+        {
+            Type = _Type;
+
+            switch (Type)
+            {
+                case SaleType.Retail:
+                    DBCode = 'R';
+                    FriendlyName = "Retail";
+                    break;
+                case SaleType.Wholesale:
+                    DBCode = 'W';
+                    FriendlyName = "Wholesale";
+                    break;
+                case SaleType.RetailOutOfState:
+                    DBCode = 'O';
+                    FriendlyName = "Retail - Out of State";
+                    break;
+                default:
+                    DBCode = ' ';
+                    FriendlyName = "None";
+                    break;
+            }
+        }
+    }
+
+    // Visual auxilliary
+    public class SalesTypeBinder
+    {
+        public Dictionary<SaleType, String> Dict = new Dictionary<SaleType, String>();
+
+        public SalesTypeBinder(ComboBox _Box)
+        {
+            foreach (SaleType type in Enum.GetValues(typeof(SaleType)))
+            {
+                Dict.Add(type, new SaleTypeInfo(type).FriendlyName);
+            }
+
+            _Box.ItemsSource = Dict;
+            _Box.DisplayMemberPath = "Value";
+            _Box.SelectedValuePath = "Key";
+            _Box.SelectedIndex = 0;
+        }
+    }
+
+    // Visual Auxilliary
+    public class FieldListItem
+    {
+        public String DisplayName = "";
+        public String DBFieldName = "";
+        public FieldType FieldType = FieldType.ftUndefined;
+        public UIElement InputElement;
+
+        public FieldListItem( String _DisplayName, String _DBFieldName, FieldType _Type, UIElement _InputElement=null )
+        {
+            DisplayName    = _DisplayName;
+            DBFieldName    = _DBFieldName;
+            FieldType      = _Type;
+
+            if( _InputElement != null )
+            {
+                InputElement = _InputElement;
+            }
+            else
+            {
+                switch (_Type)
+                {
+                    case FieldType.ftChoice:
+                        InputElement = new ComboBox();
+                        break;
+                    case FieldType.ftString:
+                    default:
+                        InputElement = new TextBox();
+                        break;
+                }
+            }
+        }
+
+        private String GetSqlType()
+        {
+            String Result = "";
+
+            switch (FieldType)
+            {
+                case FieldType.ftInt:
+                    Result = "INTEGER";
+                    break;
+                case FieldType.ftReal:
+                    Result = "REAL";
+                    break;
+                case FieldType.ftDate:
+                case FieldType.ftChoice:
+                case FieldType.ftString:
+                default:
+                    Result = "TEXT";
+                    break;
+            }
+
+            return Result;
+        }
+
+        public String FieldSql()
+        {
+            return DBFieldName + " " + GetSqlType();
+        }
+    }
+
+    public partial class SalesWindow : Window
+    {
+        private bool IsSaved = false;
+
+        public SalesWindow( List<FieldListItem> _FieldList )
+        {
+            InitializeComponent();
+            FillGrids(_FieldList);
+        }
+
+        private void FillGrids(List<FieldListItem> _FieldList)
+        {
+            // Add 4 columns
+            foreach (int ix in Enumerable.Range(0, 4))
+            {
+                FieldGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            GridLength height = new GridLength(30);
+            int CurrentRow = 0;
+            int Col0 = 0;
+
+            for (int ix = 0; ix < _FieldList.Count; ix++)
+            {
+                if(Col0 == 0 && ix > (_FieldList.Count / 2))
+                {
+                    Col0 = 2;
+                    CurrentRow = 0;
+                }
+
+                // Add a row to grid if we're in the first half
+                if (Col0 == 0)
+                {
+                    RowDefinition row = new RowDefinition();
+                    row.Height = height;
+                    FieldGrid.RowDefinitions.Add(row);
+                }
+
+                // in Col0, add a label
+                TextBlock txt = new TextBlock();
+                txt.Text = _FieldList[ix].DisplayName + ":";
+                txt.HorizontalAlignment = HorizontalAlignment.Right;
+                txt.VerticalAlignment = VerticalAlignment.Center;
+                Grid.SetRow(txt, CurrentRow);
+                Grid.SetColumn(txt, Col0);
+                FieldGrid.Children.Add(txt);
+
+                // in Col0 + 1 of the row, add a control to enter the data
+                UIElement input = _FieldList[ix].InputElement;
+                Grid.SetRow(input, CurrentRow);
+                Grid.SetColumn(input, Col0 + 1);
+                FieldGrid.Children.Add(input);
+
+                CurrentRow++;
+            }
+        }
+
+        private bool Save()
+        {
+            IsSaved = false;
+
+            if ( ! ValidateFields() )
+            {
+                MessageBox.Show("Unable to save; one or more fields not valid.");
+            }
+            else
+            {
+                //System.Data.sqllite
+                MessageBox.Show("Saving... Just kidding; that's not implemented yet.");
+                IsSaved = true;
+            }
+
+            return IsSaved;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Save();    
+        }
+
+        private void PrintInvoiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsSaved && !Save())
+            {
+                MessageBox.Show("You can't print an unsaved invoice.");
+            }
+            else
+            {
+                MessageBox.Show("Printing... Just kidding; that's not implemented yet.");
+            }
+        }
+
+        private bool ValidateFields()
+        {
+            return true;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
